ccb64ded76f612a316ee93d3fce01fdf8dc264f6
gytel420 <gyteltopka420@gmail.com>
2025-10-03 09:42:23 +0200

    3.10.2025

--- alerte.js
+++ alerte.js
@@ -1,12 +1,95 @@
 
-// var liczby = [1,2,3,4];
-// document.write ( liczby + "<br>")
-// var usuniety=liczby.pop();
-// document.write (liczby + "<br>")
-// document.write ( "element usuniety:"+ usuniety)
+// // var liczby = [1,2,3,4];
+// // document.write ( liczby + "<br>")
+// // var usuniety=liczby.pop();
+// // document.write (liczby + "<br>")
+// // document.write ( "element usuniety:"+ usuniety)
 
-let liczby = [1,2,3,4]
-document.write(liczby + "<br>")
-let usuniety = liczby.shift()
-document.write(liczby+"<br>")
-document.write("element usuniety"+ usuniety)
+// // let liczby = [1,2,3,4]
+// // document.write(liczby + "<br>")
+// // let usuniety = liczby.shift()
+// // document.write(liczby+"<br>")
+// // document.write("element usuniety"+ usuniety)
+
+// // var fruits = ["Banana","Orange","Apple","Mango"]
+// // document.write(fruits + "<br>");
+// // fruits.sort
+// // document.write("lista po sortowanieu"+ fruits + "</br>")
+// // fruits.reverse
+// // document.write("lista po odworceniu"+fruits)
+
+// // var kolejka=[];
+// // kolejka.push('jeden');
+// // kolejka.push('dwa')
+// // kolejka.push('trzy')
+// // document.write('teraz kolejka zawiera' + kolejka + '<br>')
+// // var usuniety = kolejka.shift()
+// // document.write('usunieto element ' + usuniety + '</br>' )
+// // document.write('Teraz kolejka zawiera : ' + kolejka)
+
+// // const txt ='Ala ma kota'
+// // const txt = txt.slice(0,3);
+// // console.log(txt2);// ala 
+// // const txt3=txt.slice(4)
+// // console.log(txt3) //ma kota 
+// // const txt4= txt.slice(-4)
+// // console.log(txt4) //kota
+
+
+
+// // const str = 'the quick brown fox jumps over the lazy dog.';
+
+// // // Podział tekstu na słowa
+// // const words = str.split(' ');
+// // console.log(words[3]); // "fox"
+
+// // // Podział tekstu na pojedyncze znaki
+// // const chars = str.split('');
+// // console.log(chars[8]); // "k"
+
+// // let liczba1=prompt("Podaj pierwsza liczbe");
+// // let liczba2=prompt("podaj druga liczbe");
+// // let operacja=prompt("wybierz rodzaj operacji");
+// // switch(operacja){
+// //   case "+":
+// //     wynik=liczba1+liczba2;
+// //     break;
+// //   case"-":
+// //   wynik=liczba1-liczba2;
+// //   break;
+// //   case"*":
+// //   wynik=liczba1*liczba2;
+// //   break;
+// //   case"/":
+// //   wynik=liczba1/liczba2break;
+// // break;
+// // }
+// // document.write("wynik="wynik);
+
+//parzyste liczby od 0 d0 20 
+// let i = 0;
+// while (i <= 20) {
+//   console.log(i);
+//   i += 2;
+// }
+ 
+// let i=0
+// while (i<=10) {
+//   document.write (i++ + ',')
+// }
+// od 0 do 10 w liscie html 
+// z tr jakies gowno 
+// let html1 = "<table><tr>";  
+// let i = 0;
+
+// while (i <= 10) {
+//   html1 += `<td>${i}</td>`;  
+//   i++;
+// }
+
+// html1 += "</tr></table>";  
+// document.body.innerHTML = html1;
+// reverse liczby za pomoca for 
+//  for(let i=10; i>0;--i){
+//    console.log ("liczba:"+i)
+//  }
